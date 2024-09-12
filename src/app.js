67015552aaa0e85cb61a917dde7a9250a1706296
Pedro <jprivero4@gmail.com>
2024-09-12 02:42:10 -0400

    Proyecto pendiente de input Height y Width

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = obtenerCarta;
+
+let botonNew = document.getElementById("botonNewCard");
+botonNew.addEventListener("click", obtenerCarta);
+
+let botonTimer = document.getElementById("botonTimer");
+botonTimer.addEventListener("click", () => {
+  setInterval(obtenerCarta, 5000);
+});
+
+function obtenerCarta() {
+  let elemCard = document.getElementById("card");
+  let numeroCarta = Math.floor(Math.random() * 13) + 1;
+  switch (numeroCarta) {
+    case 1:
+      numeroCarta = "A";
+      break;
+    case 11:
+      numeroCarta = "J";
+      break;
+    case 12:
+      numeroCarta = "Q";
+      break;
+    case 13:
+      numeroCarta = "K";
+      break;
+  }
+  elemCard.innerHTML = numeroCarta;
+
+  let palos = ["♦", "♥", "♠", "♣"];
+  let indexPalos = Math.floor(Math.random() * 4);
+
+  let elemsPalo = document.querySelectorAll(".palo");
+  elemsPalo.forEach(valor => {
+    valor.innerHTML = palos[indexPalos];
+    if (indexPalos < 2) {
+      valor.style.color = "red";
+      elemCard.style.color = "red";
+    } else {
+      valor.style.color = "black";
+      elemCard.style.color = "black";
+    }
+  });
+}
